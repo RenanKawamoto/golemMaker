1e91c77c4baf43d9ee624acb6f6fad66a61700dd
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-09-22 16:17:52 -0300

    Adicionado o componente de teste para ver como funciona

--- src/screens/Home.py
+++ src/screens/Home.py
@@ -1,7 +1,11 @@
-from tkinter import Frame, Label, Button
+from tkinter import Frame, Label, Button, ttk
+from src.components.GenericButton import GenericButton
+
 
 class HomeScreen(Frame):
     def __init__(self, parent):
         super().__init__(parent)
         Label(self, text="Home").pack()
+        GenericButton(self, text="BotaoGenerico", command=lambda:print("ola"), fontSize=20).pack()
         Button(self, text="About", command=lambda: parent.showScreen("About")).pack()
+        ttk.Button(self, text="ola").pack()
