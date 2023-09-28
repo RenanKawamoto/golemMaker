18d50660a4cdf47cfba5cd280457e9c63696dbe2
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-09-28 16:14:32 -0300

    Iniciado a criação basica da tela de 'Sobre'

--- src/components/screens/sobre.py
+++ src/components/screens/sobre.py
@@ -1,14 +1,18 @@
 from tkinter.ttk import Frame, Label
 from src.abstract_classes.component import Component
 from src.components.route_button import RouteButton
+from os import getenv
 
 class SobreScreen(Component, Frame):
     def __init__(self, parent):
         Component.__init__(self, parent)
         Frame.__init__(self, parent)
-        Label(self, text="Um pouco sobre o MakeGolem").pack()
-        Label(self, text="Um pequeno projeto que tem o intuito de optimizar tarefas repetitivas criando scripts para faze-las.\n Criado por: Renan Kawamoto da Rocha").pack()
+        Label(self, text="Um pouco sobre o GolemMaker", style="H1.TLabel").pack()
+        Label(self, style="Sobre.Text.TLabel", wraplength=int(getenv("WINDOW_WIDTH"))-100, text="Um pequeno projeto que tem o intuito de optimizar tarefas repetitivas criando scripts para faze-las.\n\nCriado por: Renan Kawamoto da Rocha").pack()
         RouteButton(self, text="Voltar", to_where="home").pack()
 
     def local_style_config(self):
-        ...
+        self.local_style.configure(
+            "Sobre.Text.TLabel",
+            font=("calibri", 13)
+        )
