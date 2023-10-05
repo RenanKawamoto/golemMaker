9c62088c32f765d2716f3af21da07432631fec0b
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-10-05 14:50:11 -0300

    Criado o component scrollable_canvas e feitos alguns teste na screen Home

--- src/components/screens/home.py
+++ src/components/screens/home.py
@@ -8,14 +8,12 @@ class HomeScreen(Component, Frame):
     def __init__(self, parent):
         Component.__init__(self, parent)
         Frame.__init__(self, parent)
-        ScrollableCanvas(self, width=100, height=100).place(relx=0.5, rely=0, anchor="n")
-        '''
+        
         Label(self, text="Golem Maker", style="Title.H1.TLabel").place(relx=0.5, rely=0, anchor="n")
         RouteButton(self, text="Criar", to_where="create").place(relx=0.5, rely=0.3, anchor="s")
         RouteButton(self, text="Rodar", to_where="run").place(relx=0.5, rely=0.5, anchor="s")
         RouteButton(self, text="Editar", to_where="edit").place(relx=0.5, rely=0.7, anchor="s")
         RouteButton(self, text="Sobre", to_where="about").place(relx=0.5, rely=0.9, anchor="s")
-        '''
 
     def local_style_config(self):
         self.local_style.configure(
--- src/components/scrollable_canvas.py
+++ src/components/scrollable_canvas.py
@@ -1,5 +1,5 @@
-import tkinter as tk
-from tkinter.ttk import Frame, Scrollbar, Label
+from tkinter import Canvas
+from tkinter.ttk import Frame, Scrollbar
 from src.abstract_classes.component import Component
 
 class ScrollableCanvas(Component, Frame):
@@ -7,32 +7,28 @@ class ScrollableCanvas(Component, Frame):
         Component.__init__(self, parent)
         Frame.__init__(self, parent)
         
-        self.canvas = tk.Canvas(self, width=width, height=height)
+        self.canvas = Canvas(self, width=width, height=height)
         self.canvas.pack(side="left", fill="both", expand=True)
 
         self.vertical_scrollbar = Scrollbar(self, orient="vertical", command=self.canvas.yview)
-        self.vertical_scrollbar.pack(side="right", fill="y", expand=True)
+        self.vertical_scrollbar.pack(side="right", fill="y")
 
         self.canvas.configure(yscrollcommand=self.vertical_scrollbar.set)
         
         self.inner_frame = Frame(self.canvas)
-        self.canvas.create_window((0, 0), window=self.inner_frame, anchor="nw")
         
-        for i in range(1, 100):
-            label = Label(self.inner_frame, text=i)
-            label.pack()
+        self.canvas.create_window((0, 0), window=self.inner_frame, anchor="nw")
 
-        self.update_idletasks()
+        self.canvas.bind_all("<MouseWheel>", self.on_mousewheel)
+
+    def add_component_in_canvas(self, component, *args, **kwargs):
+        _component = component(self.inner_frame, *args, **kwargs)
+        _component.pack(side="top", fill="x", expand=True)
         self.canvas.config(scrollregion=self.canvas.bbox("all"))
-
-        # Adicione eventos de rolagem do mouse
-        self.canvas.bind("<MouseWheel>", self.on_mousewheel)
-
+        self.update_idletasks()
 
     def on_mousewheel(self, event):
-        # Rolar para cima
-        if event.num == 4 or event.delta == 120:
+        if event.delta == 120:
             self.canvas.yview_scroll(-1, "units")
-        # Rolar para baixo
-        elif event.num == 5 or event.delta == -120:
+        elif event.delta == -120:
             self.canvas.yview_scroll(1, "units")
