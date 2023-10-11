b06ae04ee33ecce536d4b36a3168a80203b24f52
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-10-11 14:45:01 -0300

    Alterado a estrutura da tela Create para testes

--- src/components/screens/create.py
+++ src/components/screens/create.py
@@ -1,24 +1,34 @@
+from tkinter import StringVar
 from tkinter.ttk import Frame, Label, Button, Entry
 from src.abstract_classes.component import Component
 from src.components.route_button import RouteButton
 from src.components.scrollable_canvas import ScrollableCanvas
+from src.components.label_and_entry import LabelAndEntry
 
 class CreateScreen(Component, Frame):
     def __init__(self, parent):
         Component.__init__(self, parent)
         Frame.__init__(self, parent)
+      
+        self.scrollable_canvas_width = 350
+        self.scrollable_canvas_height = 300
+        self.entry_file_name_width = 44
+             
         
-        self.entry_file_name = Entry(self, textvariable=self.file_name, width=self.entry_file_name_width)
-        self.entry_file_name.place(relx=0.866, rely=0.113, anchor="ne")
+        self.label_and_entry = LabelAndEntry(self, label_text="Nome do golem: ", text_variable=self.file_name, entry_width=self.entry_file_name_width)
+        self.label_and_entry.place(relx=0.866, rely=0.1, anchor="ne")
+      
+        self.scrollable_canvas = ScrollableCanvas(self, width=self.scrollable_canvas_width, height=self.scrollable_canvas_height)
+        self.scrollable_canvas.place(relx=0.5, rely=0.15, anchor="n")
         
-        scrollable_canvas = ScrollableCanvas(self, width=self.scrollable_canvas_width, height=self.scrollable_canvas_height)
-        scrollable_canvas.place(relx=0.5, rely=0.15, anchor="n")
+        self.route_button_home = RouteButton(self, text="Voltar", to_where="home")
+        self.route_button_home.place(relx=0.25, rely=0.95, anchor="s")
         
-        RouteButton(self, text="Voltar", to_where="home").place(relx=0.25, rely=0.95, anchor="s")
-        Button(self, text="Criar", style="Create.TButton").place(relx=0.75, rely=0.95, anchor="s")
-        
+        self.button_create = Button(self, text="Criar", style="Create.TButton", command=lambda:print(self.file_name.get()))
+        self.button_create.place(relx=0.75, rely=0.95, anchor="s")
+                
         self.local_style_config()
-        
+          
     def local_style_config(self):
         #self.entry_file_name.configure(width=100)
         self.local_style.configure(
@@ -27,7 +37,5 @@ class CreateScreen(Component, Frame):
         )
         
     def local_vars_set(self):
-        self.file_name = ""
-        self.scrollable_canvas_width = 350
-        self.scrollable_canvas_height = 300
-        self.entry_file_name_width = 40
+        self.file_name = StringVar()
+        
