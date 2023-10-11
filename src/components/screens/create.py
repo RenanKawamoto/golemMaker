43dad7c46feede0e757bc006c958a0ba81ce6360
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-10-11 16:03:55 -0300

    Alterado a tela de Create para testar botão de adição dinamico

--- src/components/screens/create.py
+++ src/components/screens/create.py
@@ -14,12 +14,13 @@ class CreateScreen(Component, Frame):
         self.scrollable_canvas_height = 300
         self.entry_file_name_width = 44
              
-        
+             
         self.label_and_entry = LabelAndEntry(self, label_text="Nome do golem: ", text_variable=self.file_name, entry_width=self.entry_file_name_width)
         self.label_and_entry.place(relx=0.866, rely=0.1, anchor="ne")
       
         self.scrollable_canvas = ScrollableCanvas(self, width=self.scrollable_canvas_width, height=self.scrollable_canvas_height)
         self.scrollable_canvas.place(relx=0.5, rely=0.15, anchor="n")
+        self.scrollable_canvas.add_component_in_canvas_grid(Button, row=self.current_row, column=0, text="+", command=self.create_command)
         
         self.route_button_home = RouteButton(self, text="Voltar", to_where="home")
         self.route_button_home.place(relx=0.25, rely=0.95, anchor="s")
@@ -29,6 +30,10 @@ class CreateScreen(Component, Frame):
                 
         self.local_style_config()
           
+    def create_command(self):
+        self.current_row += 1  
+        self.scrollable_canvas.add_component_in_canvas_grid(Button, row=self.current_row, column=0, text="+", command=self.create_command)
+    
     def local_style_config(self):
         #self.entry_file_name.configure(width=100)
         self.local_style.configure(
@@ -38,4 +43,5 @@ class CreateScreen(Component, Frame):
         
     def local_vars_set(self):
         self.file_name = StringVar()
+        self.current_row = 0
         
