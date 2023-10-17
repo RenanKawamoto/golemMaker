91f11f9e8a5ec65846755c792f34b103df5a7dad
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-10-17 14:59:37 -0300

    refactor(golem_language_writer): Alterar construtor para passar file_name e gerar propriedades e alterar maneira como os arquivos são abertos

--- src/data/golem_language_writer.py
+++ src/data/golem_language_writer.py
@@ -1,17 +1,19 @@
 from os import getenv
 
 class GolemLanguageWriter:
-    def __init__(self):
+    def __init__(self, file_name):
         self.path = getenv("GOLENS_SCRIPTS_PATH")
         self.rows = []
+        self.file_name_with_extension = file_name + getenv("GOLENS_SCRIPTS_EXTENSIONS")
+        self.file_name = file_name
         
-    def create_golem_script(self, file_name):
-        file = open(self.path + file_name + getenv("GOLENS_SCRIPTS_EXTENSIONS"), "x")
-                       
-    def golem_script_fill(self, file_name):
-        file = open(self.path + file_name + getenv("GOLENS_SCRIPTS_EXTENSIONS"), "a")
-        for row in self.rows:
-            file.write(row)
+    def create_golem_script(self):
+        with open(self.path + self.file_name_with_extension , "x") as file:
+
+    def golem_script_fill(self):
+        with open(self.path + self.file_name_with_extension, "a") as file:
+            for row in self.rows:
+                file.write(row)
                 
     def add_command_row_click(self, x, y, time):
         self.rows.append(f"command:click/x:{x}/y:{y}/time:{time}\n")
